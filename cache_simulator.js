c0180eff55fd9d240832270478846739fb7ff17f
Daniel Pôrto Núñez <daniel.nunez@nav9.tech>
2023-04-14 15:27:48 -0300

    feat: read binary file

--- cache_simulator.js
+++ cache_simulator.js
@@ -34,65 +34,53 @@ let cache = Array.from({ length: nsets }, (_, index) => {
 
 
 
-const readStream = fs.createReadStream(arquivo_de_entrada, { encoding: 'utf8' });
-
 console.log("bits para offset: " + n_bits_offset);
 console.log("bits para index: " + n_bits_index);
 console.log("bits para tag: " + n_bits_tag);
 
-readStream.on('data', (chunk) => {
-  const lines = chunk.split('\n');
-  for (const line of lines) {
-    // Processa leitura
-		const conjunto_destino = getConjuntoDestino(line)
-		const tag_destino = parseInt(line) >> (n_bits_offset + n_bits_index); // desloca os bits do index e do offset para a direita
-		const linha_em_binario = parseInt(line).toString(2).padStart(32, '0'); // para visualizar o endereco em binario
+const fileBuffer = fs.readFileSync(arquivo_de_entrada);
 
-		const hit = cache[conjunto_destino].find((linha) => {
-			return linha.tag === tag_destino && !!linha.validade;
-		})
+for(let i = 0; i < fileBuffer.length; i += 4) {
+  const line = fileBuffer.readInt32BE(i); 															// Leitura de 4 bytes (32 bits) por vez
+  // Processa leitura
+	const conjunto_destino = getConjuntoDestino(line)											// Calcula o conjunto de destino
+	const tag_destino = parseInt(line) >> (n_bits_offset + n_bits_index); // desloca os bits do index e do offset para a direita
+	const linha_em_binario = parseInt(line).toString(2).padStart(32, '0');// para visualizar o endereco em binario
 
-		if(hit) {
-			console.log('hit', line)
-		} else {
-			console.log('miss', line)
+	const hit = cache[conjunto_destino].find((linha) => {
+		return linha.tag === tag_destino && !!linha.validade;
+	})
+
+	if(hit) {
+		hits++;
+	} else {
+		const index_via_vazia = cache[conjunto_destino].findIndex((linha) => {
+			return !linha.validade;
+		});
+
+		if(index_via_vazia !== -1) { // Miss compulsorio
+			cache[conjunto_destino][index_via_vazia].validade = true;
+			cache[conjunto_destino][index_via_vazia].tag = tag_destino;
+		} else { // Miss de capacidade / Miss de conflito   === TRATAMENTO SÓ PARA MAPEAMENTO DIRETO
+			cache[conjunto_destino][0].validade = true;
+			cache[conjunto_destino][0].tag = tag_destino;
 		}
-
-		if(hit) {
-			hits++;
-		} else {
-			const index_via_vazia = cache[conjunto_destino].findIndex((linha) => {
-				return !linha.validade;
-			});
-
-			if(index_via_vazia !== -1) { // Miss compulsorio
-				cache[conjunto_destino][index_via_vazia].validade = true;
-				cache[conjunto_destino][index_via_vazia].tag = tag_destino;
-			} else { // Miss de capacidade / Miss de conflito   === TRATAMENTO SÓ PARA MAPEAMENTO DIRETO
-				cache[conjunto_destino][0].validade = true;
-				cache[conjunto_destino][0].tag = tag_destino;
-			}
-			misses++;
-		}
-
-		acessos++;
+		misses++;
 	}
-});
 
-readStream.on('end', () => {
-	const miss_rate = misses / acessos;
-	const hit_rate = hits / acessos;
-	console.log('hits', hits)
-	console.log('misses', misses)
-	
-  console.log('miss_rate: ', miss_rate);
-	console.log('hit_rate', hit_rate)
-	console.log('acessos', acessos)
-});
+	acessos++;
+}
 
-readStream.on('error', (err) => {
-  console.error(err);
-});
+
+
+const miss_rate = misses / acessos;
+const hit_rate = hits / acessos;
+console.log('hits', hits)
+console.log('misses', misses)
+
+console.log('miss_rate: ', miss_rate);
+console.log('hit_rate', hit_rate)
+console.log('acessos', acessos)
 
 
 function getConjuntoDestino(endereco) {
